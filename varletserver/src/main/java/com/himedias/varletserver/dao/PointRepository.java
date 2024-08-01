717058c90c19700d439bda4a68424bd0cad645e0
태영 <xodud6809@naver.com>
2024-08-01 18:12:49 +0900

    member
    create member entity

---
+++ varletserver/src/main/java/com/himedias/varletserver/dao/PointRepository.java
@@ -0,0 +1,7 @@
+package com.himedias.varletserver.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PointRepository {
+}
--- varletserver/src/main/java/com/himedias/varletserver/service/PointService.java
+++ varletserver/src/main/java/com/himedias/varletserver/service/PointService.java
@@ -1,5 +1,6 @@
 package com.himedias.varletserver.service;
 
+import com.himedias.varletserver.dao.PointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PointService {
