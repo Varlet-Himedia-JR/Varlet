82b46c4220dc7a1a4c2c37ab1e9b545fb90424d3
umyunho <kolgu96@naver.com>
2024-08-07 14:41:13 +0900

    re
    re

--- varletclient/src/component/request/RCommunityList.js
+++ varletclient/src/component/request/RCommunityList.js
@@ -1,5 +1,4 @@
 import React, { useState,useEffect } from 'react'
-
 import axios from 'axios'
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
--- varletclient/src/tailwind.config.js
+++ varletclient/src/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/component/request/.{html,js}"],
     theme: {
       extend: {},
     },
