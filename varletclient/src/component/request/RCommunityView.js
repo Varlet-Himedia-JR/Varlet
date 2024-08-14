2dfbd9305cb8bf2e61400c7519cedce8e237ace2
umyunho <kolgu96@naver.com>
2024-08-14 11:17:30 +0900

    view,location retouch

--- varletclient/src/component/request/RCommunityView.js
+++ varletclient/src/component/request/RCommunityView.js
@@ -425,6 +425,19 @@ const location2Data = {
       navigate(`/rCommunityDetail/${rnum}/rcCommunityWrite`);
     };
   
+    const [showDates, setShowDates] = useState(false);
+
+    const toggleShowDates = () => {
+      setShowDates(!showDates);
+    };
+
+    const [showLocation, setShowLocation] = useState(false);
+
+  const toggleShowLocation = () => {
+    setShowLocation(!showLocation);
+  };    
+
+
 return (
       
 
@@ -514,25 +527,35 @@ return (
   <div class="prose prose-lg">
     <div className="bg-gray-100 p-8 rounded-lg mb-8">
       <div>
-        <div class="mr-4 text-xl">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="w-5 h-5 mr-1 inline-block"
-          >
-            <path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"></path>
-            <path d="M15 5.764v15"></path>
-            <path d="M9 3.236v15"></path>
-          </svg>
-          여행 예상지: {getLocationName(post.location, post.location2)}
+      <div>
+      <div class="mr-4 text-2xl font-bold mb-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-5 h-5 mr-1 inline-block"
+        >
+          <path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"></path>
+          <path d="M15 5.764v15"></path>
+          <path d="M9 3.236v15"></path>
+        </svg>
+        <span onClick={toggleShowLocation} style={{ cursor: 'pointer' }}>
+          여행 예상지:(클릭하여 확인)
+        </span>
+      </div>
+
+      {showLocation && (
+        <div className="text-xl">
+          {getLocationName(post.location, post.location2)}
         </div>
+      )}
+    </div>
       </div>
       <div class="mr-4 text-2xl font-bold mb-2">
         <svg
@@ -552,9 +575,19 @@ return (
           <rect width="18" height="18" x="3" y="4" rx="2"></rect>
           <path d="M3 10h18"></path>
         </svg>
-        여행 기간: {getTravelDuration(post.startdate, post.enddate)}
-      </div>
-      
+        <span onClick={toggleShowDates} style={{ cursor: 'pointer' }}>
+          상세 여행 일정:(클릭하여 확인)
+        </span>
+            </div>
+      <div>
+      {showDates && (
+        <div className="text-xl">
+          여행 시작일: {extractDate(post.startdate)} <br />
+          여행 종료일: {extractDate(post.enddate)}  <br />
+          총 여행 일수: {getTravelDuration(post.startdate, post.enddate)} 
+        </div>
+      )}
+    </div>
       <div className="text-lg leading-relaxed min-h-[40rem] w-full">
         <pre className="whitespace-pre-wrap">{post.content}</pre>
       </div>
