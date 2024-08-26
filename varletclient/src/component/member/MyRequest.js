285c910a392f8fa98bca4cbc61f96c01e6a41f0d
umyunho <kolgu96@naver.com>
2024-08-26 14:18:03 +0900

    ㄴㅇㄹㄴㅇ

--- varletclient/src/component/member/MyRequest.js
+++ varletclient/src/component/member/MyRequest.js
@@ -211,11 +211,11 @@ const MyRequest = () => {
                             aria-disabled={currentPage >= totalPages - pageRange}
                         >
                             <span className="sr-only">10 Pages Forward</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-player-track-next" width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M3 19v-14l8 -7z" />
-                                <path d="M14 19v-14l8 -7z" />
-                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-track-next" width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M3 5v14l8 -7z" />
+        <path d="M14 5v14l8 -7z" />
+      </svg>
                         </a>
                     </li>
                 </ul>
